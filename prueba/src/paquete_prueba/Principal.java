c32c8f5a6d6760ea7ad3aeb17082fc890ab29035
jesus <jesus@dominio.com>
2023-09-07 19:20:49 +0200

    add get /set

--- prueba/src/paquete_prueba/Principal.java
+++ prueba/src/paquete_prueba/Principal.java
@@ -8,6 +8,19 @@ public class Principal {
 	}
 
 	int edad;
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
 	String nombre;
 	
 	
