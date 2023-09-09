c3ce2799e38ebbeac568b25554cd901cc69aa2e3
jesuswp2 <jcr41530almacenamiento@gmail.com>
2023-09-09 23:19:06 +0200

    cambios222

--- prueba/src/cambio1/Cambio1.java
+++ prueba/src/cambio1/Cambio1.java
@@ -5,6 +5,7 @@ public class Cambio1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(" cambios rama git cambios1");
+		
 	}
 	
 
@@ -12,5 +13,6 @@ public class Cambio1 {
 
 public static void cambios2() {
     System.out.println("cambios rama git cambios2");
+    System.out.println("cambios rama git cambios2biss");
 }
 }
